35c77643151633066dfcd29482d643a215057e72
Ubuntu <ubuntu@ip-172-31-22-255.us-east-2.compute.internal>
2021-12-06 17:20:54 +0000

    Fixed react problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,25 @@
-import { useState } from "react";
-import ReactDOM from "react-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(inputs);
-  }
-
   return (
-    // <form onSubmit={handleSubmit} action="/form">
-    <form action="/form">
-      <label for="BaseURI"> Enter BaseURI: </label><br/>
-      <input 
-        id="BaseURI"
-        type="text" 
-        name="BaseURI" 
-        value={inputs.BaseURI || ""} 
-        onChange={handleChange}
-      /><br/>
-      <label for="APPKey">Enter APPKey: </label><br/>
-        <input 
-          id="APPKey"
-          type="text" 
-          name="APPKey" 
-          value={inputs.APPKey || ""} 
-          onChange={handleChange}
-        /><br/>
-      <label for="AuthToken">Enter AuthToken: </label><br/>
-        <input 
-          id="AuthToken"
-          type="text" 
-          name="AuthToken" 
-          value={inputs.AuthToken || ""} 
-          onChange={handleChange}
-        /><br/>
-      <label for="PKey">Enter PKey: </label><br/>
-        <input 
-          id="PKey"
-          type="text" 
-          name="PKey" 
-          value={inputs.PKey || ""} 
-          onChange={handleChange}
-        /><br/>
-        <input type="submit" />
-    </form>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-export default App; 
+export default App;
